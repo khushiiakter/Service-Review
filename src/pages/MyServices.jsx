f86d9c3eab3bbff7031cd4f1c4b3d4b78239fbbc
khushiiakter <rkjia1311@gmail.com>
2024-12-25 19:31:31 +0600

    added design in my services page

--- src/pages/MyServices.jsx
+++ src/pages/MyServices.jsx
@@ -129,13 +129,11 @@ const MyServices = () => {
   );
   return (
     <section className="container px-4 mx-auto pt-12">
-      <div className="flex items-center gap-x-3">
-        <h2 className="text-lg font-medium text-gray-800 ">My Posted Jobs</h2>
+      <div className="flex items-center gap-x-5">
+        <h2 className="text-3xl font-bold text-gray-800 ">My Posted Services ({services.length}) </h2>
         
 
-        <span className="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full ">
-          {services.length} Job
-        </span>
+        
 
         <label className="input  input-bordered w-96 flex items-center  gap-2">
           <input type="text" value={searchQuery}
@@ -156,16 +154,16 @@ const MyServices = () => {
       </div>
 
 
-      <div className="flex flex-col mt-6">
-        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-            <div className="overflow-hidden border border-gray-200  md:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-200">
-                <thead className="bg-gray-50">
+      <div className="flex flex-col  mt-6">
+        <div className="  ">
+          <div className="w-full py-2  ">
+            <div className=" border border-gray-200  md:rounded-lg">
+              <table className="w-full divide-y divide-gray-200">
+                <thead className="">
                   <tr>
                     <th
                       scope="col"
-                      className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500"
+                      className="pl-6  text-xl font-bold text-left text-gray-800"
                     >
                       <div className="flex items-center gap-x-3">
                         <span>Title</span>
@@ -174,88 +172,71 @@ const MyServices = () => {
 
                     <th
                       scope="col"
-                      className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500"
+                      className="px-4 py-2 text-xl font-bold text-left text-gray-800"
                     >
                       <span>Price</span>
                     </th>
 
                     <th
                       scope="col"
-                      className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500"
+                      className="px-4  text-xl font-bold text-left text-gray-800"
                     >
                       Category
                     </th>
                     <th
                       scope="col"
-                      className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500"
+                      className="px-4  text-xl font-bold text-left text-gray-800"
                     >
-                      Description
+                      Company Name
+                    </th>
+                    <th className="px-4  text-xl font-bold text-left text-gray-800">
+                      Posted Date
+                    </th>
+                    <th className="px-4  text-xl font-bold text-left text-gray-800">
+                     Edit Options
                     </th>
 
-                    <th className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500">
-                      Edit
-                    </th>
                   </tr>
+                    
                 </thead>
-                <tbody className="bg-white divide-y divide-gray-200 ">
+                <tbody className="bg-white divide-y divide-gray-300 ">
                   {/* Generate dynamic tr */}
                   {filteredServices.map((service) => (
                     <tr key={service._id}>
-                      <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">
+                      <td className="pl-6 py-4 text-lg text-gray-600 font-medium whitespace-nowrap">
                         {service.serviceTitle}
                       </td>
 
-                      <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">
+                      <td className="px-4 py-4 text-lg text-gray-600  whitespace-nowrap">
                         ${service.price}
                       </td>
-                      <td className="px-4 py-4 text-sm whitespace-nowrap">
+                      <td className="px-4 py-4 text-lg text-gray-600  whitespace-nowrap">
                         <div className="flex items-center gap-x-2">
                           <p>{service.category}</p>
                         </div>
                       </td>
-                      <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">
-                        {service.description.substring(0, 18)}...
+                      <td className="px-4 py-4 text-lg text-gray-600  whitespace-nowrap">
+                        {service.companyName}
                       </td>
-                      <td className="px-4 py-4 text-sm whitespace-nowrap">
-                        <div className="flex items-center gap-x-6">
+                      <td className="px-4 py-4 text-lg text-gray-600  whitespace-nowrap">
+                        {service.addedDate}
+                      </td>
+                      <td className="px-4 py-4 text-lg whitespace-nowrap">
+                        <div className="flex items-center gap-x-10">
                           <button
                             onClick={() => handleDelete(service._id)}
-                            className="text-gray-500 transition-colors duration-200   hover:text-red-500 focus:outline-none"
+                            className="text-gray-600 transition-colors duration-200 font-semibold  hover:text-red-500 focus:outline-none"
                           >
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              strokeWidth="1.5"
-                              stroke="currentColor"
-                              className="w-5 h-5"
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                              />
-                            </svg>
+                           
+                             Delete
                           </button>
 
                           <button
                             onClick={() => handleEdit(service)}
-                            className="text-gray-500 transition-colors duration-200   hover:text-yellow-500 focus:outline-none"
+                            className="text-gray-600 transition-colors duration-200  font-semibold   hover:text-gray-800 focus:outline-none"
                           >
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              strokeWidth="1.5"
-                              stroke="currentColor"
-                              className="w-5 h-5"
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-                              />
-                            </svg>
+                            
+                              Update
                           </button>
                         </div>
                       </td>
@@ -267,9 +248,9 @@ const MyServices = () => {
           </div>
           {isModalOpen && selectedService && (
             <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-              <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-xl">
-                <h2 className="text-2xl font-bold mb-4">Update Service</h2>
-                <form onSubmit={handleUpdate} className="space-y-6">
+              <div className="bg-white p-6 rounded-lg  shadow-lg w-full max-w-2xl">
+                <h2 className="text-2xl font-bold text-center mb-4">Update Service</h2>
+                <form onSubmit={handleUpdate} className="space-y-2">
                   {/* Form Fields */}
                   <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                     <div>
@@ -280,10 +261,10 @@ const MyServices = () => {
                         type="text"
                         name="serviceTitle"
                         defaultValue={selectedService.serviceTitle}
-                        className="w-full  px-3 py-2 border rounded shadow"
+                        className="input input-bordered w-full"
                       />
                     </div>
-                    <div className="mb-4">
+                    <div className="">
                       <label
                         className="block text-sm font-medium mb-1"
                         htmlFor="companyName"
@@ -299,7 +280,7 @@ const MyServices = () => {
                       />
                     </div>
                   </div>
-                  <div className="mb-4">
+                  <div className="">
                     <label
                       className="block text-sm font-medium mb-1"
                       htmlFor="website"
@@ -314,7 +295,7 @@ const MyServices = () => {
                       className="input input-bordered w-full"
                     />
                   </div>
-                  <div className="mb-4">
+                  <div className="">
                     <label
                       className="block text-sm font-medium mb-1"
                       htmlFor="description"
@@ -328,7 +309,7 @@ const MyServices = () => {
                       className="textarea textarea-bordered w-full"
                     />
                   </div>
-                  <div className="mb-4">
+                  <div className="">
                     <label
                       className="block text-sm font-medium mb-1"
                       htmlFor="category"
@@ -343,7 +324,7 @@ const MyServices = () => {
                       className="input input-bordered w-full"
                     />
                   </div>
-                  <div className="mb-4">
+                  <div className="">
                     <label
                       className="block text-sm font-medium mb-1"
                       htmlFor="price"
@@ -361,12 +342,12 @@ const MyServices = () => {
                   <div className="flex justify-end gap-2">
                     <button
                       type="button"
-                      className="btn btn-secondary"
+                      className="text-white bg-[#0F1035] hover:text-[#0F1035] hover:bg-[#0f10356c] font-semibold  px-4 py-2 rounded-xl"
                       onClick={closeModal}
                     >
                       Cancel
                     </button>
-                    <button type="submit" className="btn btn-primary">
+                    <button type="submit" className="text-white bg-[#0F1035] hover:text-[#0F1035] hover:bg-[#0f10356c] font-semibold  px-4 py-2 rounded-xl ">
                       Update
                     </button>
                   </div>
